ec52490780a9faf61608e006155950a5a4f0ba5c
Manu <manulopez18899@gmail.com>
2024-04-15 23:55:51 +0200

    Tere perdona a Segio x2

--- P3/src/cenaGala.cpp
+++ P3/src/cenaGala.cpp
@@ -5,17 +5,17 @@
 using namespace std;
 
 // Función para leer la matriz desde un archivo
-void leerMatrizDesdeArchivo(const string& nombreArchivo, vector<vector<int>> & afinidades) {
+int leerMatrizDesdeArchivo(const string& nombreArchivo, vector<vector<int>> & afinidades) {
     ifstream archivo(nombreArchivo);  // Abrir el archivo en modo lectura
 
     if (!archivo.is_open()) {
         cout << "No se pudo abrir el archivo " << nombreArchivo << endl;
-        return;
+        return 0;
     }
 
     int numeroFilas;
     archivo >> numeroFilas;  // Leer el número de filas desde el archivo
-
+    
      afinidades.resize(numeroFilas, vector<int>(numeroFilas, -1));  // Crear la matriz
 
     // Leer los valores de la matriz desde el archivo
@@ -25,8 +25,10 @@ void leerMatrizDesdeArchivo(const string& nombreArchivo, vector<vector<int>> & a
             archivo >> afinidades[i][j];
         }
     }
-    
+
     archivo.close();  // Cerrar el archivo
+   
+    return numeroFilas;
 
 }
 
@@ -40,45 +42,68 @@ int crearMesa(const vector<vector<int>> & matrizAfinidad, vector<vector<int>> &a
 
     int afinidadTotal = 0; 
 
+    
+
     while(candidatos.size() > 0){
 
         int alumno = *candidatos.begin();
         candidatos.erase(alumno);
-	bool cambio1 = false;
-	bool cambio2 = false;
-	
-	for (int m = 0; m < 2; m++){
-        int mayorAfinidad = 0; 
-        int candidato = -1;
-
-        for(auto j = candidatos.begin();  j != candidatos.end(); j++){
-            int afinidad = matrizAfinidad[alumno][*j] + matrizAfinidad[*j][alumno];
-
-            if ( afinidad > mayorAfinidad){
-                mayorAfinidad = afinidad;
-                candidato = *j;
-            }
-        }
-	
-	for (int m = 0; m < 2; m++){
-		if(adyacentes[alumno][m] == 0 && cambio1 == false){
-        		adyacentes[alumno][m] = candidato;
-			cambio1 = true;
-		}
-
-		if(adyacentes[candidato][m] == 0 && cambio2 == false){
-        		adyacentes[candidato][m] = alumno;
-			cambio2 = true;
-			if(m == 1)
-				candidatos.erase(candidato);
-		}
-	}
-        afinidadTotal += mayorAfinidad;
         
-	}
 
+        // for(auto it = candidatos.begin(); it != candidatos.end(); it++){
+        //     cout << *it ;
+        // }
+
+        // cout <<endl;
+	
+        for (int m = 0; m < 2; m++){
+            bool cambio1 = false;
+        bool cambio2 = false;
+            int mayorAfinidad = 0; 
+            int candidato = -1;
+            
+            for(auto j = candidatos.begin();  j != candidatos.end(); j++){
+                int afinidad = matrizAfinidad[alumno][*j] + matrizAfinidad[*j][alumno];
+
+                if ( afinidad > mayorAfinidad && adyacentes[alumno][0] != *j){
+                    mayorAfinidad = afinidad;
+                    candidato = *j;
+                }
+               
+            }
+        
+            for (int x = 0; x < 2; x++){
+                
+                if(adyacentes[alumno][x] == -1 && cambio1 == false){
+                        adyacentes[alumno][x] = candidato;
+                    cambio1 = true;
+                }
+                
+                if(adyacentes[candidato][x] == -1 && cambio2 == false){
+                        adyacentes[candidato][x] = alumno;
+                    
+                    cambio2 = true;
+                    if(x == 1){
+                    
+                        candidatos.erase(candidato);
+                        
+                    }
+                }
+                
+            }
+                afinidadTotal += mayorAfinidad;
+                
+        }
+
+        // for(int i = 0; i < adyacentes.size(); i++){
+        //     for(int j = 0; j < adyacentes[0].size(); j++){
+        //         cout << adyacentes[i][j] << " ";
+        //     }
+        //     cout <<endl;
+        // }
+        // cout <<endl;
     }
-
+    
     return afinidadTotal;
 }
 
@@ -87,39 +112,44 @@ int main(int argc, char* argv[]) {
         cout << "Uso: " << argv[0] << " afinidades.txt" << endl;
         return 1;  // Salir del programa con un código de error
     }
-
+    
     string nombreArchivo = argv[1];
-
     vector<vector<int>> afinidades;
-    leerMatrizDesdeArchivo(nombreArchivo, afinidades);
+    
+     int numeroFilas = leerMatrizDesdeArchivo(nombreArchivo, afinidades);
+    
 
-    vector<vector<int>> adyacentes;
+     vector<vector<int>> adyacentes;
+   
+    adyacentes.resize(numeroFilas, vector<int>(2, -1));
 
-    for (int i = 0; i < afinidades.size(); ++i) {
 
-        for (int j = 0; j < 2; ++j) {
-            adyacentes[i][j]= 0;
-        }
-    }
+    // for( int i = 0; i < numeroFilas; i++){
+	//     for( int j = 0; j < adyacentes[i].size(); j++){
+    //     	cout << adyacentes[i][j] << " ";
+	//     }
+    // }
 
     int afinidadTotal = crearMesa(afinidades,adyacentes);
+    
 
-    // // Mostrar la matriz en la consola
+    // // // Mostrar la matriz en la consola
    
-    // for (long unsigned int i = 0; i < afinidades.size(); ++i) {
-    //     for (long unsigned int j = 0; j < afinidades[i].size(); ++j) {
-    //         cout << afinidades[i][j] << " ";
-    //     }
-    //     cout << endl;
-    // }
+    // // for (long unsigned int i = 0; i < afinidades.size(); ++i) {
+    // //     for (long unsigned int j = 0; j < afinidades[i].size(); ++j) {
+    // //         cout << afinidades[i][j] << " ";
+    // //     }
+    // //     cout << endl;
+    // // }
 
 
     //Mostrar las personas que se encuentran adyacentes a cada candidato
 
-    for(long unsigned int i = 0; i < adyacentes.size(); i++){
-	for(long unsigned int j = 0; i < 2; j++){
-        	cout << adyacentes[i][j] << " ";
-	}
+    for( int i = 0; i < adyacentes.size(); i++){
+        for( int j = 0; j < adyacentes[0].size(); j++){
+                cout << adyacentes[i][j] << " ";
+        }
+        cout <<endl;
     }
 
     cout << endl << "Afinidad total: " << afinidadTotal <<endl;
